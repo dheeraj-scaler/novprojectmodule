e37c7a2f97313907b2e740e4116234cfcebb1ec2
Default User <defaultuser@Defaults-MacBook-Air.local>
2024-12-05 22:47:59 +0530

    Basic setup done

---
+++ src/main/java/com/scaler/novprojectmodule/models/Category.java
@@ -0,0 +1,30 @@
+package com.scaler.novprojectmodule.models;
+
+public class Category {
+    private Long id;
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Category() {
+    }
+
+    public Category(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+}
