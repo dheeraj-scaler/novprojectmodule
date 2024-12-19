2bfeb66bb76db7f640a2eb8582a59213cc9ad155
Default User <defaultuser@Defaults-MacBook-Air.local>
2024-12-19 22:22:14 +0530

    Fetch types

--- src/main/java/com/scaler/novprojectmodule/models/Category.java
+++ src/main/java/com/scaler/novprojectmodule/models/Category.java
@@ -1,10 +1,14 @@
 package com.scaler.novprojectmodule.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -14,11 +18,22 @@ import lombok.Setter;
 public class Category extends BaseModel{
     private String title;
 
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
+    private List<Product> products;
+
     public String getTitle() {
         return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
     }
 }
--- src/test/java/com/scaler/novprojectmodule/NovProjectModuleApplicationTests.java
+++ src/test/java/com/scaler/novprojectmodule/NovProjectModuleApplicationTests.java
@@ -1,6 +1,8 @@
 package com.scaler.novprojectmodule;
 
+import com.scaler.novprojectmodule.models.Category;
 import com.scaler.novprojectmodule.models.Product;
+import com.scaler.novprojectmodule.repository.CategoryRepository;
 import com.scaler.novprojectmodule.repository.ProductRepository;
 import com.scaler.novprojectmodule.repository.projections.ProductProjection;
 import org.junit.jupiter.api.Test;
@@ -15,6 +17,9 @@ class NovProjectModuleApplicationTests {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Test
     void contextLoads() {
@@ -31,6 +36,23 @@ class NovProjectModuleApplicationTests {
         List<ProductProjection> productProjectionList = productRepository.getProductByCategoryIdUsingProjections(1L);
         System.out.println(productProjectionList.get(0).getTitle());
 
+    }
+
+    @Test
+    void fetchTypeTest() {
+//        Category cat = categoryRepository.findById(1L).get();
+//        System.out.println(cat.getId());
+//        System.out.println("We are done here");
+//
+//        List<Product> currentProduct = cat.getProducts();
+//        System.out.println(currentProduct.size());
+//
+//        System.out.println("We got the products as well");
+
+
+       // HW , try to create a scenario for N+1
+        // Hint : Fetch list of category
+        // Iterate and try to do getProduct()
     }
 
 }
