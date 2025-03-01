471e729618e2b8fcf0a1c4d1ca40d68dab276508
Default User <defaultuser@Defaults-MacBook-Air.local>
2025-03-01 22:59:52 +0530

    Builder final

--- src/main/java/com/lld/dp/builder/Client.java
+++ src/main/java/com/lld/dp/builder/Client.java
@@ -3,15 +3,24 @@ package com.lld.dp.builder;
 public class Client {
 
     public static void main(String[] args) {
-         Builder b = new Builder();
-         b.setName("Anmol");
-         b.setAge(18);
-         b.setGradYear(2020);
-         b.setPhoneNumber("7870745678");
+//         Builder b = new Builder();
+//         b.setName("Anmol");
+//         b.setAge(18);
+//         b.setGradYear(2020);
+//         b.setPhoneNumber("7870745678");
+//
+//         //Student s = new Student(b);
+//
+//        Student s = b.build();
 
-         //Student s = new Student(b);
 
-        Student s = b.build();
+        Student s = Student.Builder.getBuilder()
+                .setName("Dipesh")
+                .setAge(19)
+                .build();
+
+
+                // set name and age
 
     }
 }
--- src/main/java/com/lld/dp/builder/Student.java
+++ src/main/java/com/lld/dp/builder/Student.java
@@ -10,13 +10,9 @@ public class Student {
     String phoneNumber;
     String email;
 
-    public Student(Builder builder) {
-        if(builder.getGradYear() >= 2022) {
-            throw new RuntimeException("Grad year should be less than 2022");
-        }
+    // make this private
+    private Student(Builder builder) {
 
-
-        // do all the validations here
 
         this.name = builder.getName();
         this.age = builder.getAge();
@@ -24,5 +20,99 @@ public class Student {
         this.psp = builder.getPsp();
         this.gradYear = builder.getGradYear();
         this.phoneNumber = builder.getPhoneNumber();
+    }
+
+
+
+    public static class Builder {
+
+        String name;
+        int age;
+        String batch;
+        double psp;
+        int gradYear;
+        String phoneNumber;
+        String email;
+
+        public String getName() {
+            return name;
+        }
+
+        // return type as Builder in order to support chain while setting the values
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public String getBatch() {
+            return batch;
+        }
+
+        public Builder setBatch(String batch) {
+            this.batch = batch;
+            return this;
+        }
+
+        public double getPsp() {
+            return psp;
+        }
+
+        public Builder setPsp(double psp) {
+            this.psp = psp;
+            return this;
+        }
+
+        public int getGradYear() {
+            return gradYear;
+        }
+
+        public Builder setGradYear(int gradYear) {
+            this.gradYear = gradYear;
+            return this;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public Builder setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        // responsible for validations and calling student cons
+        public Student build() {
+            // cons of student with the attr of builder
+            if(this.getGradYear() >= 2022) {
+                throw new RuntimeException("Grad year should be less than 2022");
+            }
+
+
+            // do all the validations here
+
+            return new Student(this);
+        }
+
+        public static Builder getBuilder() {
+            return new Builder();
+        }
     }
 }
